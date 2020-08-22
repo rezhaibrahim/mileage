4a7153a145ad8ca2ceb2472a62828641762fa879
rezhaibrahim <rezhaibrahim96@gmail.com>
2020-08-23 02:52:35 +0700

    fixed newtime

--- App.js
+++ App.js
@@ -3,4 +3,4 @@ const mencari = require('./src/Function');
 const convertM= mencari.jarakTempuh_Mobil('10:12:21','12:00:00',6)/1000;
 
 console.log(`Jarak Tempuh : ${mencari.jarakTempuh_Mobil('10:12:21','12:00:00',6)} m`);
-console.log(`Jarak Tempuh :${convertM} km`);
+console.log(`Jarak Tempuh : ${convertM} km`);
